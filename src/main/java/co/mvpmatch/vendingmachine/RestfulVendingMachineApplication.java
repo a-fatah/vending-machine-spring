eef1437d24a52c6965bbba3487b4cdd94c1482ef
a-fatah <abdulfatah66@gmail.com>
2024-02-14 23:14:11 +0100

    initial commit

---
+++ src/main/java/co/mvpmatch/vendingmachine/RestfulVendingMachineApplication.java
@@ -0,0 +1,13 @@
+package co.mvpmatch.vendingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulVendingMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulVendingMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/mvpmatch/vendingmachine/RestfulVendingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mvpmatch.vendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulVendingMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
